f3d87a4c59f538a5047c94eb0d5cf84d13217025
Adam Morgan <amorgan@netrixllc.com>
2023-11-28 11:40:08 -0500

    update for Stephen, including Chery's new block of code

--- app/celery/__init__.py
+++ app/celery/__init__.py
@@ -8,9 +8,9 @@ def celery_init_app(app: Flask) -> Celery:
         def __call__(self, *args: object, **kwargs: object) -> object:
             with app.app_context():
                 return self.run(*args, **kwargs)
-
-    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app = Celery(app.name)
     celery_app.config_from_object(app.config["CELERY"])
+    celery_app.Task = FlaskTask
     celery_app.set_default()
     app.extensions["celery"] = celery_app
     return celery_app
